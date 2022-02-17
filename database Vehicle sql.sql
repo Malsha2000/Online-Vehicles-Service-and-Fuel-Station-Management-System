f27b2f3188d519d1b42511a2128c8cac0bbb9f7b
Malsha Jayakody <80629089+Malsha2000@users.noreply.github.com>
2022-02-17 15:03:44 +0530

    Add files via upload

---
+++ database Vehicle sql.sql
@@ -0,0 +1,155 @@
+/*product table*/
+
+CREATE TABLE product(
+  productID INT NOT NULL AUTO_INCREMENT,
+  productname VARCHAR(45) NULL,
+  description VARCHAR(255) NULL,
+  image VARCHAR(255) NULL,
+  price VARCHAR(45) NULL,
+  PRIMARY KEY (productID)
+  );
+  
+  
+insert into product values(0, 'DSI Tyers', 'DSI Resilient tyres have highest durability. In addition the steel rings of the base section ensure the perfect grip of the tyres and prevent skidding. DSI Tyres stocks a vast array of unique tyres', 'https://m.media-amazon.com/images/I/61tumbgA7PL._AC_SX425_.jpg', '10000');
+insert into product values(0, 'Engine Oil', 'Buy car engine oil from Castrol, Mobil and Petronas, with 5w30 oil and 10w40 oil in stock. Use our oil finder tool to find out which engine oil your car .Explore a plethora of high-performance.', 'https://greasemonkey.lk/wp-content/uploads/2017/10/G2033.png', '1500');
+insert into product values(0, 'BLACKFIRE Foam Booster 16 oz', 'BLACKFIRE Foam Booster is a uniquely specialized formula that, when added to any car wash soap will instantly transform it into a foaming car wash Foam Booster. ', 'https://s.yimg.com/aah/autopia/blackfire-foam-booster-21.png', '3500');
+insert into product values(0, 'TATA Tyers','TATA Resilient tyers have highest durability. The steel rings of the base section ensure the perfect grip of the tyres and prevent skidding under extreme side loads. Tata is the largest vehicle manufacturer. ', 'https://sc04.alicdn.com/kf/HLB16eDMSIfpK1RjSZFOq6y6nFXam.jpg',	'15000');
+insert into product values(0, '3D Super Pre-Soak', '3D Super Pre-Soak will allow you to achieve a much higher level of clean with your weekly maintenance wash. The high pH formula of 3D Super rest on your paint faster than your standard car shampoo.' , 'https://s.yimg.com/aah/autogeek/3d-super-pre-soak-3.gif', '3400');
+insert into product values(0, 'CARPRO Wash Box', 'The CARPRO Box Wash is ideal starter kit for anyone to give their car a proper wash. The CARPRO Wash Box comes with Reset Intensive Car Shampoo, Elixir High Gloss Quick Detailer. ' , 'https://s.yimg.com/aah/autogeek/carpro-wash-box-34.gif', '4350');
+insert into product values(0, 'DP Coating Maintenance Kit', 'This kit comes with the DP Coating Maintenance Wash that allows you wash your car in between detailing applications, that not only cleans you car but also boots existing coatings. This mitt allows for a soft.'   , 'https://s.yimg.com/aah/autogeek/dp-coating-maintenance-kit-5.gif', '2600');
+insert into product values(0, 'DP Ceramic wash', 'DP Ceramic Wash is the wash you are looking for if you want a highly hydrophobic maintenance wash. After removing, It provides a boost to your ceramic coating each time it is used. It will make every wash easier.                  ' ,  'https://s.yimg.com/aah/autogeek/dp-ceramic-wash-12.gif', '2000');
+insert into product values(0, 'DP Ultimate Wash-N-Max', 'DP Ultimate Wash-N-Wax is the time saver you?ve been looking for. Washing your car is a  job all on its own,  you have a large car and waxing is a whole other beast. is safe on your surface.'   , 	'https://s.yimg.com/aah/autogeek/dp-wash-n-wax-5.gif', '3200');
+insert into product values(0, 'Laugfs Tyres', 'Laugh tyres may be more expensive compared to competitor tyres. Identifying the business potential in this lucrative industry, LAUGFS diversified into the rubber industry. Whilst manufacturing press on. '   ,  'https://m.media-amazon.com/images/I/61e+PSt3AeL._AC_SS450_.jpg',	'12500');
+insert into product values(0, 'Laugfs Gas', 'Making convenience the key we offer the domestic consumer the ideal pack cylinders for their energy needs. The pioneer of innovation in the industry. also offers a broad portfolio of maritime services.',	'https://buyabans.com/upload/product/Laugfs%20Gas/LPLGOTEMCYLINDER125/1_1.jpg', '1650');
+insert into product values(0, 'Litro Gas', '   Established in 1872. It was a wholly owned subsidiary of Royal Dutch Shell until 2010, when it was purchased by the government of Sri Lanka.it was a wholly owned subsidiary of Royal Dutch Shell.',       	' http://www.sundaytimes.lk/210418/uploads/litro_15042021_B05_CMY.jpg', '1500');
+insert into product values(0, 'Dash Car Wash', 'Dash Car Wash is a blend of carefully selected high quality, biodegradable surfactants with a rich proactive foam that efficiently removes dirt and road grime from vehicles. This caustic and phosphate-free formula leaves no stains on paintwork of vehicle.',	'https://multichemi-int.com/wp-content/uploads/2020/05/Car-Wash-1.png','1450');
+insert into product values(0, 'DP Wash Bundle', 'When it comes to car washing, sometimes all you need are the basics, those of which come in the DP Wash Bundle. First you need the DP Car Shampoo that cleans your car with soapy, sudsy goodness with contaminant cleaning power.' ,  	'https://s.yimg.com/aah/autogeek/dp-wash-bundle-5.gif',	'875');
+insert into product values(0, 'Tornador Sudsy Exterior', 'Tornador Sudsy Exterior makes washing your vehicle a breeze as it produces an abundance of foam with a rich, long-lasting lather. Tornador Sudsy Exterior creates high-lubricity that aids in rinsing and drying time. Your vehicle will feel slick with shine.',	'https://s.yimg.com/aah/autogeek/tornador-sudsy-exterior-5.gif','1100');
+insert into product values(0, 'Grit Guard Washboard', 'The Grit Guard Insert has revolutionized the way car care enthusiasts around the world wash their cars. The Grit Guard Washboard, which is designed to be used in conjunction with the Grit Guard Insert.', 'https://sep.yimg.com/ay/autogeek/grit-guard-washboard-red-15.gif','1990');
+insert into product values(0, 'Car Wash Bucket Kits', 'By research we mean that, over time, every car in the Autogeek parking lot got cleaned with a different combination of products and they were all paired off and bundled together to make the best car wash bucket kits available.', 'https://sep.yimg.com/ay/autogeek/car-wash-bucket-kits-19.gif','4875');
+insert into product values(0, 'BLACKFIRE Wash & Dry System', 'Perfect your car wash technique with the BLACKFIRE Wash & Dry System. This kit comes with the 5-gallon Black Bucket that allows you to fill the bucket with the perfect amount of water and product.' , 'https://s.yimg.com/aah/autogeek/blackfire-wash-dry-system-5.gif',	'8500');
+insert into product values(0, 'BLACKFIRE Wash System with Dolly', 'Before you start the much needed and most common step in the detailing process, car washing, important that you have the basic tools to begin. It is necessary for product and soaking water.'    ,     'https://s.yimg.com/aah/autogeek/blackfire-wash-system-with-dolly-5.gif',	'6500');
+insert into product values(0, 'Kenotek Brilliant Wash', 'Kenotek Brilliant Wash is a superior car wash that will be able to effectively remove dirt and grime from your car?s paint. Kenotek Brilliant Wash was developed using a PH-neutral formula, that ensures you donot have to worry about damage.'    , '	https://sep.yimg.com/ay/autogeek/kenotek-brilliant-wash-3.gif',	'1300');
+	
+    
+/*payment table*/
+    
+CREATE TABLE payment (
+  id INT NOT NULL AUTO_INCREMENT,
+  cname VARCHAR(45) NULL,
+  cnum VARCHAR(45) NULL,
+  date VARCHAR(10) NULL,
+  cvv VARCHAR(45) NULL,
+  PRIMARY KEY (id));
+  
+  
+ insert into payment values(0 , 'Credit card' , '123456789123' , '10/21' , '089');
+ insert into payment values(0 , 'Credit card' , '456789012342' , '10/14' , '145');
+ insert into payment values(0 , 'VISA' , '478596325897' , '09/23' , '753');
+ insert into payment values(0 , 'Paypal' , '741014785225' , '09/04' , '417');
+ insert into payment values(0 , 'Master Card' , '564523987210' , '08/16' , '225');
+ insert into payment values(0 , 'VISA' , '564238715946' , '09/12' , '738');
+ insert into payment values(0 , 'Paypal' , '475896582047' , '08/24' , '369');
+ insert into payment values(0 , 'Paypal' , '471485236985' , '10/25' , '457');
+ insert into payment values(0 , 'Master Card' , '763892517369' , '08/15' , '709');
+ 
+
+
+
+/*customer table*/
+
+create table customer(
+ID int(5) PRIMARY KEY,
+Name varchar(20),
+email varchar(30),
+phone int (10),
+username varchar(20),
+password varchar(20)
+
+);
+
+
+insert into customer values('111', 'Kamal', '123@gmail.com', '0709867554', 'CD006', '123int');
+insert into customer values('113', 'Thimira', 'thimira@gmail.com', '0719983343', 'CD002', 'thi221');
+insert into customer values('116', 'Malsha', 'Mal@gmail.com', '0765543343', 'CD008', '12345');
+insert into customer values('117', 'Heshi', 'heshi@gmail.com', '0705814226', 'CD007', '456789');
+insert into customer values('119', 'Pasindu', 'pasindu@gmail.com', '0705626478', 'CD010', '111qqq');
+
+
+/*emp table*/
+
+create table emp(
+id int primary key auto_increment,
+fname varchar(50),
+lname varchar(50),
+address varchar(50),
+phone varchar(15),
+NIC varchar(15),
+email varchar(50),
+username varchar(50),
+passwords varchar(50)
+);
+
+
+insert into emp values(0,'Mahela',	'jayawardana',	'gampaha',	'0147852369',	'123456789',	'Mahela@j.com',	'C002',	'asdf');
+insert into emp values(0,'Peter',	'jayakodi',	'gampaha',	'0147856923',	'01234567985',	'sasindu@gmail.com',	'C005',	'qaz');
+insert into emp values(0, 'heshi',	'sandanika',	'gampaha',	'0123456789',	'01234567985',	'luciferboy10539@gmail.com',	'C006',	'bnm');
+insert into emp values(0, 	'Elon',	'Musk',	'New York',	'0147856923',	'01234567985',	'elon@e.com',	'C009',	'java');
+insert into emp values(0, 	'Tony', 	'Stark',	'New York',	'0147856923',	'01234567985',	'tony@t.com',	'C013',	'tony');
+
+
+
+
+/*reservation table*/
+
+CREATE TABLE reservation (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(45) NULL,
+  email VARCHAR(45) NULL,
+  address VARCHAR(100) NULL,
+  vmodel VARCHAR(45) NULL,
+  phone VARCHAR(10) NULL,
+  date DATE NULL,
+  time TIME NULL,
+  service VARCHAR(200) NULL,
+  PRIMARY KEY (id));
+  
+  
+  
+  insert into reservation values(0, 'Heshani Sandanika', 'hes123@gmail.com', 'Shanthi Street, Waliweriya', 'TOYOTA KDH 200', '0774200161',	'2021-09-30',	'08:00:00',	'Oil & Filter Change , Exterior cleaning');
+  insert into reservation values(0, 'Heshani Sandanika', 'hes@gmail.com',	'Kirindiwela, Gampaha',	'Maruti suzuki Alto',	'0774235161',	'2021-09-01',	'08:00:00',	'Oil & Filter Change , Exterior cleaning');
+  insert into reservation values(0, 'Veen Srimantha',	'veen@gmail.com',	'Ja-Ela Road, Gampaha',	'Diffender',	'0763249181',	'2021-11-04',	'14:59:00',	'Brake oil change , Brake cleaner');
+  insert into reservation values(0, 'Vindiya Dewmin', 'vin123@gmail.com', 'Kings Street, Dekatana', 'TOYOTA KDH 200', '0774300161',	'2021-09-30',	'08:00:00',	'Diesel filter change , Engine scanning , Gear Oil change (Manual / Auto) ');
+  insert into reservation values(0, 'Sasindu Yasas', 'sasi@gmail.com',	'Papiliyawela, Gampaha',	'Maruti suzuki Alto',	'0772746516',	'2021-10-01',	'08:30:00',	'Interior cleaning');
+
+
+/*contactus table*/
+
+CREATE TABLE contactus (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(60) NULL,
+  email VARCHAR(45) NULL,
+  contactus VARCHAR(255) NULL,
+  message VARCHAR(255) NULL,
+  PRIMARY KEY (id));
+  
+  
+  insert into contactus values(0, 'Heshani Sandanika', 'ash@gmail.com',	'about service','your service is excellent');
+  insert into contactus values(0, 'Malsha Jayakodi', 'leng@gmail.com',	'about service','Thank You for your great service.');
+  insert into contactus values(0, 'xiaoki leng','ash@gmail.com','about service','Thank you for your service.');
+  insert into contactus values(0, 'Vindiya Dewmin', 'vin123@gmail.com', 'Your Service', 'Thank you for your service and it is a pleasure to find a place like this.');
+  insert into contactus values(0, 'Sasindu Yasas', 'sasi@gmail.com','Great service','your service is excellent');
+
+
+SELECT * FROM product;
+
+SELECT * FROM payment;
+
+SELECT * FROM customer;
+
+SELECT * FROM emp;
+
+SELECT * FROM reservation;
+
+SELECT * FROM contactus;
+
